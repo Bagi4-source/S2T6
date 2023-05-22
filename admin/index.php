b516b9493853263c61b6f1096c85e798ac80df37
Bagi4 <67656353+Bagi4-source@users.noreply.github.com>
2023-05-23 00:05:07 +0300

    update table.php

--- admin/index.php
+++ admin/index.php
@@ -22,7 +22,7 @@ $pass = '9294062';
 $db = new PDO('mysql:host=localhost;dbname=u52803', $user, $pass, [PDO::ATTR_PERSISTENT => true]);
 $users = $db->query("SELECT * FROM users;");
 
-$stmt = $db->prepare("SELECT count(id) as count FROM relations where ability_id = ?;");
+$stmt = $db->prepare("SELECT count(id) as count FROM relations group by ability_id;");
 $stmt->execute(['1']);
 foreach ($stmt as $row) {
     print($row['count']);
